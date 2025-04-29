1e899a760e6a42d9ea8139c2d389d6f487d27670
Muhammed Jimoh <m.jimoh@getkole.co>
2025-04-29 13:33:22 +0100

    feat: s3

---
+++ terraform/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "company_emr_bucket" {
+  bucket = "company-pyspark-scripts223"
+}
+
+resource "aws_s3_bucket" "source_bucket" {
+  bucket = "source-bucket-telecos-223"
+}
+
+resource "aws_s3_bucket" "cleaned_bucket" {
+  bucket = "cleaned-bucket-telecos-223"
+}
